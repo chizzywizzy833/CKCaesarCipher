bd14ad62df7ecbe6f375920b3f3de3ac9f6a6fa4
Chisomo <107890763+chizzywizzy833@users.noreply.github.com>
2023-07-08 21:04:52 +1200

    Final encryption (1-20)

--- 03_encrypt_v5.py
+++ 03_encrypt_v5.py
@@ -7,12 +7,23 @@ v4- turn v3 into a function
 v5-Get user to only enter positive numbers between 1 and 20
 """
 
-# returns the ordinal number for capital letter 'Z' Which is 90
-LAST_LETTER_CODE = ord("Z")
-ALPHABET_RANGE = 26
-
 
 def encryption():
+    # returns the ordinal number for capital letter 'Z' Which is 90
+    LAST_LETTER_CODE = ord("Z")
+    ALPHABET_RANGE = 26
+    user_message = input("What is your message you would like to encrypt: ")
+    while True:
+        try:
+            shift = int(input("enter shift value: "))
+            # get user to enter a number between 1 and 20
+            if shift < 1 or shift > 20:
+                print("Please enter a positive number between 1 and 20.")
+            else:
+                break
+            # if user puts in an unexpected at all they will be asked to put in an integer
+        except ValueError:
+            print("Invalid input. Please enter a positive number between 1 and 20.")
     result = ""
     for letter in user_message.upper():
         if letter.isalpha():
@@ -29,18 +40,5 @@ def encryption():
 
 
 # main routine
-user_message = input("What is your message you would like to encrypt: ")
-
-while True:
-    try:
-        shift = int(input("enter shift value: "))
-        # get user to enter a number between 1 and 20
-        if shift < 1 or shift > 20:
-            print("Invalid input. Please enter a positive number between 1 and 20.")
-        else:
-            break
-        # if user puts in an unexpected at all they will be asked to put in an integer
-    except ValueError:
-        print("Invalid input. Please enter a positive number between 1 and 20.")
 
 encryption()
